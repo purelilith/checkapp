6389d28c52351a2a880b84a623b7963985506d5d
purelilith <bozonhiggsovich@gmail.com>
2026-03-13 18:09:01 +0300

    fix: resolve layout issues and update resources

--- java/com/benefind/checkapp/SecondActivity.kt
+++ java/com/benefind/checkapp/SecondActivity.kt
@@ -3,14 +3,15 @@ package com.benefind.checkapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
-        val button_to_main = findViewById<Button>(R.id.buttonToMain)
-        val button_to_OCR = findViewById<Button>(R.id.buttonToOCR)
+        val button_to_main = findViewById<ImageButton>(R.id.buttonToMain)
+        val button_to_OCR = findViewById< ImageButton>(R.id.buttonToOCR)
         button_to_main.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
